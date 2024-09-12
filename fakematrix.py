04f6aeb5207c056189161189d1f4c6dfac53382d
Ray Lennon <raylennon27@gmail.com>
2024-09-11 20:22:31 -0400

    who knows

---
+++ fakematrix.py
@@ -0,0 +1,12 @@
+# Leaving this as a maybe-later... don't really feel like emulating an entire graphics library right now
+# for the sake of a silly scroller. 
+
+class RGBMatrix:
+    def __init__(self, options):
+        self.options = options
+
+        
+class RGBMatrixOptions:
+    def __init__(self, options):
+        self.options = options
+
---
+++ main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from PIL import Image
+import time
+import random
+import os
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+
+from rgbmatrix import graphics
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+
+delay = 0.02
+options = RGBMatrixOptions()
+options.rows = 32
+options.cols = 64
+options.chain_length = 1
+options.parallel = 1
+options.gpio_slowdown = 2
+options.brightness = 20
+options.hardware_mapping = "adafruit-hat"
+options.daemon = False
+options.drop_privileges = False
+
+matrix = RGBMatrix(options=options)
+
+offscreen_canvas = matrix.CreateFrameCanvas()
+
+font = graphics.Font()
+font.LoadFont("fonts/10x20.bdf")
+
+textColor = graphics.Color(255, 255, 255)
+pos = offscreen_canvas.width
+
+file = open("./text_to_scroll.txt", "r")
+lines = file.readlines()
+
+out = []
+for line in lines:
+
+    pos = offscreen_canvas.width
+
+    length = 0
+    elength = 0
+
+    while pos + length + elength > 0:
+
+        offscreen_canvas.Clear()
+        length = graphics.DrawText(offscreen_canvas, font, pos, 20, textColor, line)
+        pos -= 1
+        time.sleep(delay)
+        offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+
+
+    matrix.Clear()
